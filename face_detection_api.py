68d55e904f4749e2b547916229df2cb922e9aa2d
Hardik Modha <hardikmodha.dev@gmail.com>
2024-06-08 21:06:37 +0530

    feat: custom port based on .env

--- face_detection_api.py
+++ face_detection_api.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import dlib
 import numpy as np
@@ -33,4 +35,4 @@ def detect_faces():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.environ.get("APP_PYTHON_PORT"))
